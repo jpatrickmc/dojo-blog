3b4f2d13d01ea11f98d1fd2fa75eef7a91d778f2
Patrick McDaniel <dfwjpm@outlook.com>
2026-04-22 15:35:12 -0500

    add handleDelete

---
+++ src/BlogList.jsx
@@ -0,0 +1,22 @@
+const BlogList = ({ blogs, title, handleDelete }) => {
+  return (
+    <div className="blog-list">
+      <div>{title}</div>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <button
+            onClick={() => {
+              handleDelete(blog.id);
+            }}
+          >
+            delete blog
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogList;
