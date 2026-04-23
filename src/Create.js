54a2112e01ab312cdbcf1bf6a8facc6f5a732eda
Patrick McDaniel <dfwjpm@outlook.com>
2026-04-23 12:19:04 -0500

    add react-router; add useEffect cleanup

---
+++ src/Create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+  return (
+    <div className="Create">
+      <h2>Create a New Blog</h2>
+    </div>
+  );
+};
+
+export default Create;
--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,9 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortController = new AbortController();
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortController.signal })
         .then((res) => {
           if (!res.ok) {
             throw Error('Could not fetch the data for that resource');
@@ -20,10 +21,17 @@ const useFetch = (url) => {
           setError(null);
         })
         .catch((err) => {
-          setError(err.message);
-          setIsPending(false);
+          if (err.name === 'AbortError') {
+            console.log('Fetch aborted');
+          } else {
+            setIsPending(false);
+            setError(err.message);
+          }
         });
     }, 1000);
+    return () => {
+      abortController.abort();
+    };
   }, [url]);
 
   return { data, isPending, error };
